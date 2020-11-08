98dcb90add315c0b0cf1bf80d311b6262e0e22e0
Luc Leonard <github@luc-leonard.fr>
2020-11-08 12:06:57 +0100

    refacto(demo-basic/producer): we now use a with block.

--- demo-basic/producer.py
+++ demo-basic/producer.py
@@ -2,15 +2,14 @@ import pika
 
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='hello')
-    for i in range(0, 100):
-        channel.basic_publish(exchange='',
-                              routing_key='hello',
-                              body=f'{i}')
-    print(" [x] Sent 'Hello World!'")
-    connection.close()
+    with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as connection:
+        channel = connection.channel()
+        channel.queue_declare(queue='hello')
+        for i in range(0, 100):
+            channel.basic_publish(exchange='',
+                                  routing_key='hello',
+                                  body=f'{i}'.encode())
+            print(i)
 
 
 if __name__ == '__main__':

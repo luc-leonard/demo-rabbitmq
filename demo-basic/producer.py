c5b89148c31fa1ace4d6286ac1afe925c3af651e
Luc Leonard <github@luc-leonard.fr>
2020-11-08 14:46:27 +0100

    add stuff for CI

--- demo-basic/producer.py
+++ demo-basic/producer.py
@@ -2,16 +2,13 @@ import pika
 
 
 def main():
-    with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as connection:
+    with pika.BlockingConnection(pika.ConnectionParameters("localhost")) as connection:
         channel = connection.channel()
-        channel.queue_declare(queue='hello')
+        channel.queue_declare(queue="hello")
         for i in range(0, 100):
-            channel.basic_publish(exchange='',
-                                  routing_key='hello',
-                                  body=f'{i}'.encode())
+            channel.basic_publish(exchange="", routing_key="hello", body=f"{i}".encode())
             print(i)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
